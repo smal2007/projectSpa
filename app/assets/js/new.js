327d49ae797160167bec45bbf49dddb9a0b26624
Smal2007 <smal_2007@mail.ru>
2016-10-03 07:58:18 +0000

    my site

--- app/assets/js/new.js
+++ app/assets/js/new.js
@@ -1,12 +1,180 @@
-app.currentModule = (function($){
+app.currentModule = (function($) {
+
+
+
     return {
         init: function(obj, callback) {
+            var categoryId;
+            var price;
+            console.log(categoryId)
             console.log("Инициализируем модуль главной страницы");
             obj = obj || new Object(null);
             callback = callback || function() {
                 return false;
             }
-            
+
+
+            var categoryId;
+            $.ajax({
+                url: 'https://api.backendless.com/v1/data/category',
+                headers: {
+                    "application-id": "005B0AD0-3D76-48F4-FF85-296C0438F200",
+                    "secret-key": "3A2A9558-A762-E6A6-FF7D-51D1C5AA3200"
+                },
+                type: 'GET',
+                dataType: 'json',
+                success: function(data) {
+                    $('.list').html('');
+                    var prependURI = 'where=categoryId.objectId%3D%27';
+                    var appendURI = '%27';
+                    var categories = data.data;
+                    // Создаем первый ел.списка для пролучения всех категорий товаров. Вызываем getData БЕЗ аргумента с названием категории.
+                    /*   var li = $(' <li id="li_all_categories" class="btn btn-default active">все категории</li>');
+                       $('.list').append(li);
+                       li.on('click', function() {
+                               $('.list li').removeClass('active');
+                               $(this).addClass('active');
+                               categoryId = '';
+                               getData();
+                           });*/
+                    //Создаем список в ДОМ имеющися категорий
+                    for (var i = -1; i < categories.length; i++) {
+                        categories[i] = categories[i] || {};
+                        if (categories[i].name == undefined) categories[i].name = 'все категории';
+                        if (categories[i].objectId == undefined) categories[i].objectId = '';
+                        var li = $('<li class="item btn btn-default" data-category_id ="' + categories[i].objectId + '">' + categories[i].name + '</li>');
+                        //Вешаем на каждый элемент списка получение элементов категории
+                        li.on("click", function changeCategory() {
+                            console.log($(this).text());
+                            categoryId = $(this).data('category_id');
+                            if (categoryId == '') {
+                                categoryId = '';
+                            }
+                            else {
+                                categoryId = prependURI + categoryId + appendURI
+                            }
+                            $('.list li').removeClass('active');
+                            $(this).addClass('active')
+                            getData();
+
+                        });
+
+                        $('.list').append(li);
+
+
+                    }
+                    $.ajax({
+                        url: 'https://api.backendless.com/v1/data/vendors',
+                        headers: {
+                            "application-id": "005B0AD0-3D76-48F4-FF85-296C0438F200",
+                            "secret-key": "3A2A9558-A762-E6A6-FF7D-51D1C5AA3200"
+                        },
+                        type: 'GET',
+                        dataType: 'json',
+                        success: function(data) {
+                            var vendors = data.data;
+                            for (i = 0; i < vendors.length; i++) {
+                                var li = $('<li class="vendor" data-vendor_id="' + vendors[i].objectId + '" ><label> <input name="vendors" type="radio"> ' + vendors[i].name + ' </label> </li>');
+                                $(li).on('click', function() {
+                                    console.log($(this).data('vendor_id'))
+                                })
+                                $('.vendors').append(li)
+                            }
+
+                        }
+                    })
+
+                }
+            });
+
+
+
+
+            obj.find("#btnSort").on('click', function() {
+                if (price == 'sortBy=price%20asc&') {
+                    $('#icon').removeClass();
+                    $('#icon').addClass('glyphicon glyphicon-arrow-down')
+                    price = 'sortBy=price%20desc&';
+                    getData()
+                }
+                else {
+                    $('#icon').removeClass();
+                    $('#icon').addClass('glyphicon glyphicon-arrow-up')
+                    price = 'sortBy=price%20asc&';
+                    getData()
+
+                }
+
+
+
+
+            });
+
+
+           /* function fixedEncodeURIComponent(str) {
+                return encodeURIComponent(str).replace(/[!'()*]/g, function(c) {
+                    return '%' + c.charCodeAt(0).toString(16);
+                });
+            }*/
+
+
+
+            /* obj.find('#li_all_categories').on('click', function() {
+                 $(this).addClass('active')
+                 getData()
+             })*/
+
+
+            /*var getData =*/
+            function getData() {
+                console.log(categoryId)
+
+
+                categoryId = categoryId || '';
+                price = price || '';
+                console.log(categoryId)
+                console.log(price)
+                $.ajax({
+                    url: 'https://api.backendless.com/v1/data/items?' + price + categoryId,
+                    complete: function() {
+                        console.log(categoryId)
+                    },
+                    headers: {
+                        "application-id": "005B0AD0-3D76-48F4-FF85-296C0438F200",
+                        "secret-key": "3A2A9558-A762-E6A6-FF7D-51D1C5AA3200"
+                    },
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function(data) {
+                        var dataPhones = data.data;
+                        $('.box_for_tel').html('');
+                        for (var i = 0; i < dataPhones.length; i++) {
+                            $('.box_for_tel').append('<div class="tel col-sm-4 col-md-3" data-count="' + i + '">')
+                            var tel = $('.tel')[i];
+                            $(tel).append('<div class=" tel_thumbnail thumbnail" data-toggle="modal" data-target="#myModal">');
+                            var tel_thumbnail = $('.tel_thumbnail')[i];
+                            $(tel_thumbnail).append("<img class='foto' src='" + dataPhones[i].image + "'>");
+                            $(tel_thumbnail).append('<h3>' + dataPhones[i].title + '</h3>');
+                            $(tel_thumbnail).append('<h3>' + dataPhones[i].price + ' $</h3>');
+
+                            $(tel).on('click', function() {
+                                var currentDataTel = dataPhones[$(this).data('count')]
+                                console.log();
+                                $('#myModalLabel').html(currentDataTel.title);
+                                $('.modal-body').html('<img class="modal_foto" src="' + currentDataTel.image + '">');
+                                $('.modal-body').append('<p>' + currentDataTel.fullDescription + '</p>')
+                            })
+
+
+
+                        }
+                        console.log(data.data)
+                    }
+
+                })
+            }
+            getData()
+
             callback();
         }
     }
--- app/assets/js/registration.js
+++ app/assets/js/registration.js
@@ -1,39 +1,135 @@
 app.currentModule = (function($) {
-    var cfg = {
-        url: "https://api.backendless.com/v1/data/family",
-        id: "8C75EE00-12BF-1292-FF8F-EBEDE65D5500",
-        key: "C11E5441-DCBF-0BFB-FFA6-E7CEC369C500"
-    }
+    Backendless.initApp("B08630AE-F0DB-F308-FF82-01D4899A5E00", "D666283F-99D3-A4EF-FF8F-DD9DDA74E100", "v1");
+
+    // get objectId of the logged-in user:
+    var userObjectId = Backendless.LocalCache.get("current-user-id")
+
+    // get user-token of the logged-in user:
+    var userToken = Backendless.LocalCache.get("user-token")
+    console.log(userToken)
+
+    // get current user object:
+    var userObject = Backendless.UserService.getCurrentUser();
+    console.log(userObject);
     return {
         init: function(obj, callback) {
-            console.log("Инициализируем модуль для регистрации страницы");
+            console.log("Инициализируем модуль для главной страницы");
             obj = obj || new Object(null);
             callback = callback || function() {
                 return false;
             }
 
-         
+            var butId;
+            var objData;
+            var reg_name = /.{5,10}/ig;
+            var reg_password = /\w{5,10}/ig;
 
-            obj.find("input[type=button]").off("click").on("click", function() {
-                var to_send = {
-                   login: obj.find("input[name=Login]").val(),
-                   password: obj.find("input[name=Password]").val(),
+
+
+            $(obj).find('.sign').on("click", function showForm() {
+                console.log('ok');
+                $('.form').toggle();
+                $('.choise').toggle();
+
+                if (this.tagName != 'SPAN') {
+                    $('div.form input[type = button]').val(this.innerHTML);
+                    console.log('equal span')
+                    butId = this.id;
+                    $('.box').append('<span class="icon">');
+                    $('span.icon').html('<i class="fa fa-reply" aria-hidden="true"></i>')
+                    $('span.icon').on('click', showForm)
                 }
-                $.ajax({
-                    url: "https://api.backendless.com/v1/data/grats",
-                    method: "POST",
-                    headers: {
-                        "application-id": "3FDE63EC-7557-F093-FFB3-33819E755600",
-                        "secret-key": "891D9767-23FD-D406-FF74-F645A6791700"
-                    },
-                    contentType: "application/json",
-                    data: JSON.stringify(to_send)
-                });
-            });
+                else if (this.tagName == 'SPAN') {
+                    $(this).css('display', 'none')
+                }
+            })
 
+            $(obj).find('input[type = button]').on('click', function makeObjData() {
+                console.log($('input[name = first_name]').val());
+                if ($('input[name = first_name]').val().match(reg_name) &&
+                    $('input[name = password]').val().match(reg_password)) {
+                    objData = {};
+                    objData.name = $('input[name = first_name]').val();
+                    objData.password = $('input[name = password]').val();
+                    objData = JSON.stringify(objData);
+                    if (butId == 'sign_in') {
+                        sign_in()
+                    }
+                    else if (butId == 'sign_up') {
+                        sign_up()
+                    }
+                    else console.log(butId)
+                }
+                else {
+                    alert('try again')
+                }
+
+
+            })
+
+            function sign_in() {
+                // $.ajax({
+                //     url: 'https://api.backendless.com/v1/data/registredUsers',
+                //     headers: {
+                //         'application-id': "B08630AE-F0DB-F308-FF82-01D4899A5E00",
+                //         'secret-key': "D666283F-99D3-A4EF-FF8F-DD9DDA74E100"
+                //     },
+                //     success: function(data) {
+                //         console.log(data.data)
+                //         var dataFromServer = data.data;
+
+                //         for (var i = 0; i < dataFromServer.length; i++) {
+                //             if (dataFromServer[i].name == $('input[name = first_name]').val() && dataFromServer[i].password == $('input[name = password]').val()) {
+                //                 $('.box').html('')
+                //             }
+
+                //         }
+                //     }
+                // })
+                Backendless.UserService.login($('input[name = first_name]').val(), $('input[name = password]').val(), true);
+                
+
+            }
+
+            function sign_up() {
+                // $.ajax({
+                //     url: 'https://api.backendless.com/v1/data/registredUsers',
+                //     type: 'POST',
+                //     data: objData,
+                //     contentType: 'application/json',
+                //     headers: {
+                //         'application-id': "B08630AE-F0DB-F308-FF82-01D4899A5E00",
+                //         'secret-key': "D666283F-99D3-A4EF-FF8F-DD9DDA74E100"
+                //     },
+                //     success: function(data) {
+                //         $('.box').append('<div id="reg_complete" class="alert alert-success" role="alert"></div>');
+                //         $('#reg_complete').html('Registration complete!!! Sign in please!!!')
+                //         butId = 'sign_in';
+                //         $('div.form input[type = button]').val('sign in');
+
+                //         objData.name = $('input[name = first_name]').val('');
+                //         objData.password = $('input[name = password]').val('');
+
+                //         console.log(data)
+                //     }
+                // })
+                var user = new Backendless.User();
+                user.email = $('input[name = first_name]').val();
+                user.password = $('input[name = password]').val();
+                console.log(user, Backendless);
+                Backendless.UserService.register(user);
+            }
+
+
+            console.log('load')
 
             callback();
-        }
-    }
 
+        },
+
+    }
 })(jQuery);
+
+/*$('input[type=text]').each(function() {
+    objData[$(this).attr('name')] = $(this).val()
+})*/
